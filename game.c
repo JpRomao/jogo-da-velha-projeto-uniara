8feb8d071d024431ea3bf0b43755067e536b9f7b
João Pedro Romão <ojpzinho@gmail.com>
2022-10-12 19:13:47 -0300

    Update game.c

--- game.c
+++ game.c
@@ -1,3 +1,13 @@
+/*
+  Participantes:
+    João Pedro Romão - 04722-037
+    Maria Eduarda Garcia - 04722-069
+    Ana Beatriz - 04722-
+    Isadora - 04722-
+    Lucimara Prado - 04722-
+    Erika Caiano - 04722-
+*/
+
 #include <stdio.h>
 #include "menuAndRules.h"
 
